3a09006caafcd705804a37fc7c4928d81e5d11d5
sarah <sarah.polta@gmail.com>
2022-06-22 18:09:17 +0200

    Initial commit

---
+++ UhrzeitundDatum/src/main/java/com/example/UhrzeitundDatum/UhrzeitundDatumApplication.java
@@ -0,0 +1,13 @@
+package com.example.UhrzeitundDatum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UhrzeitundDatumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UhrzeitundDatumApplication.class, args);
+	}
+
+}
---
+++ UhrzeitundDatum/src/test/java/com/example/UhrzeitundDatum/UhrzeitundDatumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.UhrzeitundDatum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UhrzeitundDatumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
